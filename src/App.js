c3a46c12c59f6c3ecdf1b7ed1b5584a77a1da623
beautylotus <beautylotus718@gmail.com>
2023-08-21 10:25:32 -0700

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+  // const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header search={search}  setSearch={setSearch} handleChange={handleChange}></Header>
     </div>
   );
 }
